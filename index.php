92101edcd39a9cc20c8a1cf95358e4cea25a9fa9
andreagavardi <andreagavardi89@gmail.com>
2021-06-23 16:13:21 +0200

    init struttura base e navbar

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google-FAQ</title>
+    <!-- fontawesom link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600&display=swap" rel="stylesheet">
+    <!-- style css -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="navbar">
+            <div class="nav-top">
+                <div class="left-side">
+                    <div class="logo">
+                        <img src="https://www.ra.camcom.gov.it/eurosportello/allegati-giuseppe/loghi/logo-google/image"
+                            alt="">
+                    </div>
+                    <span>Privacy e termini</span>
+                </div>
+                <div class="right-side">
+                    <div class="menu">
+                        <i class="fas fa-ellipsis-v"></i>
+                        <i class="fas fa-ellipsis-v"></i>
+                        <i class="fas fa-ellipsis-v"></i>
+                    </div>
+                    <div class="account">
+                        <h3>A</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="nav-bottom"></div>
+        </div>
+    </header>
+</body>
+
+</html>

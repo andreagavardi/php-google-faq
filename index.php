253f76bf64049edaa5501476c064712b1e955434
andreagavardi <andreagavardi89@gmail.com>
2021-06-23 17:46:28 +0200

    migliorato font paragrafi

--- index.php
+++ index.php
@@ -61,6 +61,9 @@ $faqs = [
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
     <!-- style css -->
     <link rel="stylesheet" href="./assets/css/style.css">
 
